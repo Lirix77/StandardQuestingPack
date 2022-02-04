5a5389c63828efeb825237aaaa89d9ae11d1bb33
Lirix77 <lirix77@yandex.by>
2022-02-05 00:25:31 +0300

    Rework all rewards gui

--- src/main/java/bq_standard/client/gui/rewards/PanelRewardXP.java
+++ src/main/java/bq_standard/client/gui/rewards/PanelRewardXP.java
@@ -5,6 +5,7 @@ import betterquesting.api2.client.gui.misc.GuiPadding;
 import betterquesting.api2.client.gui.misc.GuiTransform;
 import betterquesting.api2.client.gui.misc.IGuiRect;
 import betterquesting.api2.client.gui.panels.CanvasEmpty;
+import betterquesting.api2.client.gui.panels.CanvasMinimum;
 import betterquesting.api2.client.gui.panels.content.PanelGeneric;
 import betterquesting.api2.client.gui.panels.content.PanelTextBox;
 import betterquesting.api2.client.gui.resources.textures.ItemTexture;
@@ -15,22 +16,24 @@ import net.minecraft.init.Items;
 import net.minecraft.util.EnumChatFormatting;
 import org.lwjgl.util.vector.Vector4f;
 
-public class PanelRewardXP extends CanvasEmpty
+public class PanelRewardXP extends CanvasMinimum
 {
     private final RewardXP reward;
+    private final IGuiRect initialRect;
     
     public PanelRewardXP(IGuiRect rect, RewardXP reward)
     {
         super(rect);
         this.reward = reward;
+        initialRect = rect;
     }
     
     @Override
     public void initPanel()
     {
         super.initPanel();
-        
-        this.addPanel(new PanelGeneric(new GuiTransform(new Vector4f(0.4F, 0.5F, 0.4F, 0.5F), -32, -16, 32, 32, 0), new ItemTexture(new BigItemStack(Items.experience_bottle))));
+        int width = initialRect.getWidth();
+        this.addPanel(new PanelGeneric(new GuiTransform(new Vector4f(0F, 0F, 0F, 0F), 0, 0, 32, 32, 0), new ItemTexture(new BigItemStack(Items.experience_bottle))));
         
 		String txt2;
 		
@@ -44,7 +47,8 @@ public class PanelRewardXP extends CanvasEmpty
 		
 		txt2 += reward.levels? "L" : "XP";
 		
-        this.addPanel(new PanelTextBox(new GuiTransform(new Vector4f(0.4F, 0.5F, 0.8F, 0.5F), new GuiPadding(4, -12, 0, 0), 0), QuestTranslation.translate("bq_standard.gui.experience")).setAlignment(1).setColor(PresetColor.TEXT_MAIN.getColor()));
-        this.addPanel(new PanelTextBox(new GuiTransform(new Vector4f(0.4F, 0.5F, 0.8F, 0.5F), new GuiPadding(4, 0, 0, -16), 0), txt2).setAlignment(1).setColor(PresetColor.TEXT_MAIN.getColor()));
+        this.addPanel(new PanelTextBox(new GuiTransform(new Vector4f(0F, 0F, 0F, 0F), 36, 2, width - 36, 16, 0), QuestTranslation.translate("bq_standard.gui.experience")).setColor(PresetColor.TEXT_MAIN.getColor()));
+        this.addPanel(new PanelTextBox(new GuiTransform(new Vector4f(0F, 0F, 0F, 0F), 40, 16, width - 40, 16, 0), txt2).setAlignment(1).setColor(PresetColor.TEXT_MAIN.getColor()));
+        recalcSizes();
     }
 }
